e30d42949441a0c804b65b017fd0259115bd5032
UrakFerraz <urakmkt@yahoo.com.br>
2022-04-21 15:49:36 -0300

    ajusta config do jest

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,15 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/layouts/**/*.vue',
+    '<rootDir>/static/**/*.vue',
+    '<rootDir>/store/**/*.ts',
+    '<rootDir>/composables/**/*.ts',
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/types/**/*.ts',
   ],
   testEnvironment: 'jsdom'
 }
