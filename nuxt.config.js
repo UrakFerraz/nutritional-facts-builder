8ed2220f5b1c1f4825928b524edf9f7cd3bf8915
UrakFerraz <urakmkt@yahoo.com.br>
2022-05-11 23:48:21 -0300

    lint em nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,39 +2,42 @@ export default {
   typescript: {
     typeCheck: {
       eslint: {
-        files: './**/*.{ts,js,vue}'
-      }
-    }
+        files: './**/*.{ts,js,vue}',
+      },
+    },
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-ts',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'preconnect', href:'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Sora:wght@200;300;400;500;600;700;800&display=swap"}
-    ]
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true,
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Akshar:wght@300;400;500;600;700&family=Sora:wght@200;300;400;500;600;700;800&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    './static/css/normalize.css',
-    './static/css/main.scss'
-  ],
+  css: ['./static/css/normalize.css', './static/css/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -48,12 +51,11 @@ export default {
     '@pinia/nuxt',
   ],
   eslint: {
-    fix: true
+    fix: true,
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
@@ -65,5 +67,5 @@ export default {
         implementation: require('sass'),
       },
     },
-  }
+  },
 }
