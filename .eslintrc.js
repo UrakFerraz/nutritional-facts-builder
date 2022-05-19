56566b19ed08a093d58cc9443b6eb7f65ab3b0b6
UrakFerraz <urakmkt@yahoo.com.br>
2022-05-18 23:43:31 -0300

    adiciona regra de lint no-useless-constructor

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,5 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: { 'vue/multi-word-component-names': 0 }
+  rules: { "no-useless-constructor": "off", 'vue/multi-word-component-names': 0 }
 }
