2339fad62682f4388f7635c5868a1781ea844fa1
UrakFerraz <urakmkt@yahoo.com.br>
2022-05-27 22:25:46 -0300

    cria interface para nutritiona values

---
+++ composables/interfaces/nutritional-values.ts
@@ -0,0 +1,41 @@
+import { IngredientInterface } from './Ingredient'
+
+export interface NutritionalValues {
+  calories: IngredientInterface
+  carbohydrate: IngredientInterface
+  protein: IngredientInterface
+  totalFat: IngredientInterface
+  saturatedFat: IngredientInterface
+  transFat: IngredientInterface
+  cholesterol: IngredientInterface
+  dietaryFibers: IngredientInterface
+  sodium: IngredientInterface
+  calcium: IngredientInterface
+  biotin: IngredientInterface
+  choline: IngredientInterface
+  chrome: IngredientInterface
+  copper: IngredientInterface
+  fluorine: IngredientInterface
+  folicAcid: IngredientInterface
+  iodine: IngredientInterface
+  iron: IngredientInterface
+  magnesium: IngredientInterface
+  manganese: IngredientInterface
+  molybdenum: IngredientInterface
+  niacin: IngredientInterface
+  pantothenicAcid: IngredientInterface
+  phosphorus: IngredientInterface
+  potassium: IngredientInterface
+  riboflavin: IngredientInterface
+  saturatedFattyAcids: IngredientInterface
+  selenium: IngredientInterface
+  thiamine: IngredientInterface
+  vitaminA: IngredientInterface
+  vitaminB12: IngredientInterface
+  vitaminB6: IngredientInterface
+  vitaminC: IngredientInterface
+  vitaminD: IngredientInterface
+  vitaminE: IngredientInterface
+  vitaminK: IngredientInterface
+  zinc: IngredientInterface
+}
