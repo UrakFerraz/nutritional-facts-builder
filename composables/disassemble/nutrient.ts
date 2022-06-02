f9a367cfb8cc1e7e2e26488418d6d1da3bd5890c
UrakFerraz <urakmkt@yahoo.com.br>
2022-06-02 01:33:24 -0300

    lint

--- composables/disassemble/nutrient.ts
+++ composables/disassemble/nutrient.ts
@@ -1,12 +1,16 @@
-import { PowderInterface } from '~/composables/interfaces/powder';
+import { PowderInterface } from '~/composables/interfaces/powder'
 
 export default class NutrientDisassemble {
-  constructor(private readonly powder: PowderInterface, private readonly nutrientName: string, private readonly powderPrice: number) {}
+  constructor(
+    private readonly powder: PowderInterface,
+    private readonly nutrientName: string,
+    private readonly powderPrice: number
+  ) {}
 
   private _context = {
     nutrientInServingSize: 0,
     servingSizePrice: 0,
-    nutrientPriceInServingSize: 0
+    nutrientPriceInServingSize: 0,
   }
 
   get context() {
@@ -15,8 +19,24 @@ export default class NutrientDisassemble {
 
   disjoin() {
     const name = this.nutrientName as keyof typeof this.powder.nutrients
-    this._context.nutrientInServingSize = Number(((Number(this.powder.nutrients[name]!.value) * 100) / Number(this.powder.description.servingSize)).toFixed(2))
-    this._context.servingSizePrice = Number(((Number(this.powder.description.servingSize) * this.powderPrice) / Number(this.powder.description.size)).toFixed(2))
-    this._context.nutrientPriceInServingSize = Number(((Number(this.powder.nutrients[name]!.value) * this._context.servingSizePrice) / Number(this.powder.description.servingSize)).toFixed(2))
+    this._context.nutrientInServingSize = Number(
+      (
+        (Number(this.powder.nutrients[name]!.value) * 100) /
+        Number(this.powder.description.servingSize)
+      ).toFixed(2)
+    )
+    this._context.servingSizePrice = Number(
+      (
+        (Number(this.powder.description.servingSize) * this.powderPrice) /
+        Number(this.powder.description.size)
+      ).toFixed(2)
+    )
+    this._context.nutrientPriceInServingSize = Number(
+      (
+        (Number(this.powder.nutrients[name]!.value) *
+          this._context.servingSizePrice) /
+        Number(this.powder.description.servingSize)
+      ).toFixed(2)
+    )
   }
 }
