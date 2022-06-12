1bb2438512e8622dc36596de061adab9c5533ab7
UrakFerraz <urakmkt@yahoo.com.br>
2022-06-12 11:30:34 -0300

    refatora VD base

--- static/mocks/BR_VD.ts
+++ static/mocks/BR_VD.ts
@@ -1,12 +1,25 @@
 // Valores diários de referência de nutrientes de declaração obrigatória
-const VD = {
-  calories: { kcal: 2000, name: 'Valor Energético' },
-  carbohydrate: { grams: 300, name: 'Carboidrato' },
-  protein: { grams: 75, name: 'Proteínas' },
-  totalFat: { grams: 55, name: 'Gorduras Totais' },
-  saturatedFat: { grams: 22, name: 'Gorduras Saturadas' },
-  dietaryFibers: { grams: 25, name: 'Fibra Alimentar' },
-  sodium: { mg: 2400, name: 'Sódio' },
+
+import { NutrientsInterface } from '~/composables/interfaces/nutrients'
+
+type PowderNutrientsVD = Pick<
+  NutrientsInterface,
+  | 'calories'
+  | 'carbohydrate'
+  | 'protein'
+  | 'totalFat'
+  | 'saturatedFat'
+  | 'dietaryFibers'
+  | 'sodium'
+>
+const VD: PowderNutrientsVD = {
+  calories: { unit: 'kcal', value: 2000, name: 'Calorias' },
+  carbohydrate: { unit: 'g', value: 300, name: 'Carboidrato' },
+  protein: { unit: 'g', value: 75, name: 'Proteínas' },
+  totalFat: { unit: 'g', value: 55, name: 'Gorduras Totais' },
+  saturatedFat: { unit: 'g', value: 22, name: 'Gorduras Saturadas' },
+  dietaryFibers: { unit: 'g', value: 25, name: 'Fibra Alimentar' },
+  sodium: { unit: 'g', value: 2000, name: 'Sódio' },
 }
 
 export default VD
