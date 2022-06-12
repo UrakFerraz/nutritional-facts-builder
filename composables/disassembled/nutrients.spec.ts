5134e494c7722273a8679955b99a2febfe1e4346
UrakFerraz <urakmkt@yahoo.com.br>
2022-06-12 12:02:04 -0300

    corrige testes unitários de nutrient serving

---
+++ composables/disassembled/nutrients.spec.ts
@@ -0,0 +1,45 @@
+import CarbohydrateServing from './nutrient-serving-carbo'
+import ProteinServing from './nutrient-serving-protein'
+import powders from '~/static/mocks/powders'
+describe('deve fracionar o powder', () => {
+  it('deve criar as classes que analisa os nutrientes', () => {
+    const sutCarbo = new CarbohydrateServing(powders[0])
+    expect(sutCarbo).toBeDefined()
+    const sutProtein = new ProteinServing(powders[0])
+    expect(sutProtein).toBeDefined()
+  })
+  it('deve criar a classe e calcular o carbo', () => {
+    const sutCarbo = new CarbohydrateServing(powders[0])
+    expect(sutCarbo).toBeDefined()
+    sutCarbo.setPercentageInServingSize()
+    const convertedMock = {
+      percentageInServingSize: 12.9,
+    }
+    expect(sutCarbo.data).toEqual(convertedMock)
+  })
+  it('deve usar método estático da classe de fracionamento e calcular o carbo', () => {
+    const sutCarbo = CarbohydrateServing.getSpecification(powders[0])
+    expect(sutCarbo).toBeDefined()
+    const convertedMock = {
+      percentageInServingSize: 12.9,
+    }
+    expect(sutCarbo).toEqual(convertedMock)
+  })
+  it('deve criar a classe e calcular proteína', () => {
+    const sutProtein = new ProteinServing(powders[0])
+    expect(sutProtein).toBeDefined()
+    sutProtein.setPercentageInServingSize()
+    const convertedMock = {
+      percentageInServingSize: 77.42,
+    }
+    expect(sutProtein.data).toEqual(convertedMock)
+  })
+  it('deve usar método estático da classe de fracionamento e calcular proteína', () => {
+    const sutProtein = ProteinServing.getSpecification(powders[0])
+    expect(sutProtein).toBeDefined()
+    const convertedMock = {
+      percentageInServingSize: 77.42,
+    }
+    expect(sutProtein).toEqual(convertedMock)
+  })
+})
