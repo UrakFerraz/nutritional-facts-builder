3ce1c8ddfab59437b36702da00c006d22d2475c6
UrakFerraz <urakmkt@yahoo.com.br>
2022-06-12 22:32:41 -0300

    ajusta jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,58 @@
+import { Nuxt, Builder } from 'nuxt'
+import nuxtConfig from './nuxt.config'
+
+const resetConfig = {
+  loading: false,
+  loadingIndicator: false,
+  fetch: {
+    client: false,
+    server: false,
+  },
+  features: {
+    store: true,
+    layouts: false,
+    meta: false,
+    middleware: false,
+    transitions: false,
+    deprecations: false,
+    validate: false,
+    asyncData: false,
+    fetch: false,
+    clientOnline: false,
+    clientPrefetch: false,
+    clientUseUrl: false,
+    componentAliases: false,
+    componentClientOnly: false,
+  },
+  build: {
+    indicator: false,
+    terser: false,
+  },
+}
+
+const config = Object.assign({}, nuxtConfig, resetConfig, {
+  mode: 'spa',
+  srcDir: nuxtConfig.srcDir,
+  ignore: ['**/components/**/*', '**/layouts/**/*', '**/pages/**/*'],
+})
+
+const buildNuxt = async () => {
+  const nuxt = new Nuxt(config)
+  await new Builder(nuxt).build()
+  return nuxt
+}
+
 module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
   },
   collectCoverage: false,
   collectCoverageFrom: [
@@ -25,5 +64,11 @@ module.exports = {
     '<rootDir>/pages/**/*.vue',
     '<rootDir>/types/**/*.ts',
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+}
+
+module.exports = async () => {
+  const nuxt = await buildNuxt()
+
+  process.env.buildDir = nuxt.options.buildDir
 }
