1c0731f0ccdeb7e5e4d09e297683f131208feb2e
UrakFerraz <urakmkt@yahoo.com.br>
2022-07-10 17:02:37 -0300

    remove editores de nutrientes substituídos pelo iterator

--- modules/nutrients/nutrient-named-base.ts
+++
@@ -1,5 +0,0 @@
-import { NutrientsInterface } from '~/interfaces/nutrients'
-
-export default abstract class NutrientNamedAbstract {
-  abstract addName(nutrientName: string, nutrients: NutrientsInterface): void
-}
--- modules/nutrients/nutrients-named-base.ts
+++
@@ -1,4 +0,0 @@
-import { NutrientsInterface } from '~/interfaces/nutrients'
-export default abstract class NutrientsNamedAbstract {
-  abstract addNames(nutrients: NutrientsInterface): void
-}
